562db7b59e6aad7e0cb59fcba07a16a74d2b997a
Farzin Bagheri <silverman2009@live.com>
2016-05-25 10:04:57 +0430

    Insert Customer Info Complated

--- Concrete/DB.cs
+++ Concrete/DB.cs
@@ -14,33 +14,137 @@ namespace Concrete
 
         public DB()
         {
-            con.ConnectionString = "server=(local);database=concreteBB; Integrated Security=SSPI";
+            con.ConnectionString = "server=(local);database=concreteBB;Integrated Security=True";
             cmd.Connection = con;
-            
         }
-
-
-
 
 
 
         public bool InserCustomer(Customer c)
         {
-            cmd.CommandText = "INSERT INTO Customer (Customer_Name,Customer_Family,Customer_ID_code) values (@FirstName , @LastName , @IDCode)";
-            cmd.Parameters.Add("@FirstName", c.Firstname);
-            cmd.Parameters.Add("@LastName", c.LastName);
-            cmd.Parameters.Add("@IDCode", c.IDCode);
-
             Open();
 
-            int count = cmd.ExecuteNonQuery();
+            SqlTransaction transaction = null;
+            transaction = con.BeginTransaction("Insert phones Transaction");
+            cmd.Transaction = transaction;
 
+
+            int id = InsertCustomerBasicInfo(c);
+            if (id > 0)
+            {
+                if (insertCustomerPhones(c, id))
+                    if (insertCustomerAddress(c, id))
+                    {
+                        transaction.Commit();
+                        Close();
+                        return true;
+                    }
+                        
+            }
+
+            transaction.Rollback();
             Close();
-            
-            if (count > 0)
-                return true;
-
             return false;
+            
+        }
+
+
+        private int InsertCustomerBasicInfo(Customer c)
+        {
+            cmd.CommandText = "INSERT INTO Customer (Customer_Name,Customer_Family,Customer_ID_code) OUTPUT INSERTED.Customer_ID values (@FirstName , @LastName , @IDCode)";
+            cmd.Parameters.AddWithValue("@FirstName", c.Firstname);
+            cmd.Parameters.AddWithValue("@LastName", c.LastName);
+            cmd.Parameters.AddWithValue("@IDCode", c.IDCode);
+
+            int id = -1;
+
+            try
+            {
+               
+
+                id = int.Parse( cmd.ExecuteScalar().ToString() );
+                insertCustomerPhones(c, id);
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+            finally
+            {
+                
+            }
+
+            return id;
+        }
+
+        public bool insertCustomerPhones(Customer c , int CustomerID)
+        {
+            cmd.CommandText = "INSERT INTO Customer_Phone (FK_Customer_ID,PhoneNumber) OUTPUT INSERTED.FK_Customer_ID values ( @FK_Customer_ID , @PhoneNumber)";
+
+            int count=-1;
+            
+            try
+            {
+                Open();
+
+                foreach (string phone in c.Phones)
+                {
+                    cmd.Parameters.AddWithValue("@FK_Customer_ID", CustomerID);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", phone);
+                    
+                    count = int.Parse(cmd.ExecuteScalar().ToString());
+
+                    if (count < 0)
+                        return false;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool insertCustomerAddress(Customer c, int CustomerID)
+        {
+            cmd.CommandText = "INSERT INTO Customer_Phone (FK_Customer_ID,PhoneNumber) OUTPUT INSERTED.FK_Customer_ID values ( @FK_Customer_ID , @Address)";
+
+            SqlTransaction transaction = null;
+
+            int count = -1;
+
+            try
+            {
+                Open();
+
+                transaction = con.BeginTransaction("Insert phones Transaction");
+
+                cmd.Transaction = transaction;
+
+                foreach (string address in c.Addresses)
+                {
+                    cmd.Parameters.AddWithValue("@FK_Customer_ID", CustomerID);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", address);
+
+                    count = int.Parse(cmd.ExecuteScalar().ToString());
+
+                    if (count < 0)
+                        return false;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                return false;
+            }
+
+            transaction.Commit();
+            Close();
+
+            return true;
         }
 
         public bool EditCustomer(Customer c)
@@ -63,14 +167,63 @@ namespace Concrete
             throw new NotImplementedException();
         }
 
+        private int getCustomerID(string _Firstname, String _LastName)
+        {
+            cmd.CommandText = string.Format("select Customer_ID from Customer where Customer_Name = '{0}' and Customer_Family = '{1}'", _Firstname, _LastName);
+            int id=-1;
+            try
+            {
+                Open();
+                id = int.Parse( cmd.ExecuteScalar().ToString());
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                Close();
+            }
+
+            return id;
+        }
+
+        /// <summary>
+        /// get Customer By ID_Code
+        /// </summary>
+        /// <param name="Customer_ID_code"></param>
+        /// <returns></returns>
+        private int getCustomerID(string Customer_ID_code)
+        {
+            cmd.CommandText = string.Format("select Customer_ID from Customer where Customer_ID_code = '{0}'", Customer_ID_code);
+            int id = -1;
+            try
+            {
+                Open();
+                id = int.Parse(cmd.ExecuteScalar().ToString());
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                Close();
+            }
+
+            return id;
+        }
+
         public void Open()
         {
-            con.Open();
+            if (con.State != System.Data.ConnectionState.Open)
+                con.Open();
         }
 
         public void Close()
         {
-            con.Close();
+            if(con.State ==System.Data.ConnectionState.Open)
+                con.Close();
         }
     }
 
@@ -89,5 +242,6 @@ namespace Concrete
         void Open();
 
         void Close();
+        
     }
 }

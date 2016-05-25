025686aa8a96c79b25c5cd2a7e4ee5b1fae5e84d
Farzin Bagheri <silverman2009@live.com>
2016-05-26 00:20:34 +0430

    Class Added

---
+++ Concrete/Career.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Concrete
+{
+    public class Career
+    {
+        public Career(string _CareerDriverFName, string _CareerDriverLName, string _plaque, string _ownership)
+        {
+            CareerDriverFName = _CareerDriverFName;
+            CareerDriverLName = _CareerDriverLName;
+            plaque = _plaque;
+            ownership = _ownership;
+        }
+        
+        public string CareerDriverFName { get;set; }
+
+        public string CareerDriverLName { get;set;}
+
+        public string plaque { get; set; }
+
+        public string ownership { get; set; }
+
+        public string CareerID { get; set; }
+    }
+}

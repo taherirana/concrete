e21c446e5962aff7365658728aeb81cab7b12a4b
Farzin Bagheri <silverman2009@live.com>
2016-05-25 13:53:54 +0430

    added

---
+++ Concrete/Concrete.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Concrete
+{
+    public class Concrete
+    {
+        public Concrete(int _Type, int _Price)
+        {
+            Type = _Type;
+            Price = _Price;
+        }
+
+        public int Type
+        {
+            get;
+            set;
+        }
+
+        public int Price
+        {
+            get;
+            set;
+        }
+    }
+}

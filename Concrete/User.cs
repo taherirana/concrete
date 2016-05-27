cce6e6b3fcfe953a5a4eb6e91d753ff8fa984bce
Farzin Bagheri <silverman2009@live.com>
2016-05-27 21:26:21 +0430

    User Class Added

---
+++ Concrete/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Concrete
+{
+    public class User
+    {
+
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public DateTime LastLoginDate { get; set; }
+        public String FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}

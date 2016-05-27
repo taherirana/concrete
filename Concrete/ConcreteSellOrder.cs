8b4f337e9afb5ceef099139905b49a538506ce38
Farzin Bagheri <silverman2009@live.com>
2016-05-27 23:59:41 +0430

    New Class Added ConcreteSellOrder Class => collection Of sellItem SellItem => SellItem Detail

---
+++ Concrete/ConcreteSellOrder.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Concrete
+{
+    public class ConcreteSellOrder : ICollection<SellItem>
+    {
+        public static User user;
+
+        List<SellItem> _sellItms;
+
+        public ConcreteSellOrder()
+        {
+            _sellItms = new List<SellItem>();
+        }
+
+
+        public void Add(SellItem item)
+        {
+            _sellItms.Add(item);
+        }
+
+        public void Clear()
+        {
+            _sellItms.Clear();
+        }
+
+        public bool Contains(SellItem item)
+        {
+            return _sellItms.Contains(item);
+        }
+
+        public void CopyTo(SellItem[] array, int arrayIndex)
+        {
+            //Not Implemened
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _sellItms.Count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        public bool Remove(SellItem item)
+        {
+            return _sellItms.Remove(item);
+        }
+
+        public IEnumerator<SellItem> GetEnumerator()
+        {
+            return _sellItms.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _sellItms.GetEnumerator();
+        }
+    }
+}
---
+++ Concrete/SellItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Concrete
+{
+    public class SellItem
+    {
+        public string CustomerName { get; set; }
+        public string MixerDriverName { get; set; }
+        public string CareeDriverName { get; set; }
+        public string ConcreteType { get; set; }
+        public string Operation { get; set; }
+        public string Address { get; set; }
+
+        public float WeightInTon { get; set; }
+        public float WeightInM2 { get; set; }
+        public float price { get; set; }
+
+
+
+
+        public SellItem(string _CustomerName, string _MixerDriverName, string _CareeDriverName, string _ConcreteType, string _Operation, string _Address, float _WeightInTon, float _WeightInM2, float _price)
+        {
+            CustomerName = _CustomerName;
+            MixerDriverName = _MixerDriverName;
+            CareeDriverName = _CareeDriverName;
+            ConcreteType = _ConcreteType;
+            Operation = _Operation;
+            Address = _Address;
+
+            WeightInTon = _WeightInTon;
+            WeightInM2 = _WeightInM2;
+            price = _price;
+        }
+    }
+}

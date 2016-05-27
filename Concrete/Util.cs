771ce13617580026f6de6a0548e767c86f482440
Farzin Bagheri <silverman2009@live.com>
2016-05-27 22:38:01 +0430

    Util Class Added

---
+++ Concrete/Util.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Concrete
+{
+    public class Util
+    {
+        public User user;
+    }
+}
